9a760e269d47f147576ccc7c0f7b6bd05f725cce
Margarita <bogatova.md@gmail.com>
2021-05-21 11:54:58 +0300

    preprocess to rf record

--- configs/by_device_type/tpu_dir_config.py
+++ configs/by_device_type/tpu_dir_config.py
@@ -1,13 +1,13 @@
 from configs.common_constants import EXP_NAME
 
-CHECKPOINT_DIR = "gs://transformedd/experiments/" + EXP_NAME + "/checkpoint/"
+CHECKPOINT_DIR = "gs://ramas_tpu/experiments/" + EXP_NAME + "/checkpoint/"
 CHECKPOINT_NAME = "cp-{epoch:04d}.ckpt"
 
-TENSORBOARD_DIR = "gs://transformedd/experiments/" + EXP_NAME + "/tb"
+TENSORBOARD_DIR = "gs://ramas_tpu/experiments/" + EXP_NAME + "/tb"
 TENSORBOARD_NAME = 'epoch-{}'
 
-# DATASET_PATH = "/content/drive/MyDrive/RAMAS"
-DATASET_TF_RECORDS_PATH = "gs://transformedd/transformer"
+DATASET_PATH = "/content/drive/MyDrive/RAMAS"
+DATASET_TF_RECORDS_PATH = "gs://ramas_tpu/tf_records/full"
 
 PRETRAINED_MODELS = "/content/drive/MyDrive/pretrained/"
 
