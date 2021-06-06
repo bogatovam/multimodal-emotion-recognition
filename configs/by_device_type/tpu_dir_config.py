183f218b3588341f8900c9d4770fdee1c0d76108
Margarita <bogatova.md@gmail.com>
2021-06-06 11:39:50 +0300

    Add dropout

--- configs/by_device_type/tpu_dir_config.py
+++ configs/by_device_type/tpu_dir_config.py
@@ -7,7 +7,7 @@ TENSORBOARD_DIR = "gs://ramas_tpu_na/experiments/" + EXP_NAME + "/tb"
 TENSORBOARD_NAME = 'epoch-{}'
 
 DATASET_PATH = "/content/drive/MyDrive/RAMAS"
-DATASET_TF_RECORDS_PATH = "/content/drive/MyDrive/ramas_tpu/tf_records/final"
+DATASET_TF_RECORDS_PATH = "/content/drive/MyDrive/ramas_features"
 
 PRETRAINED_MODELS = "/content/drive/MyDrive/pretrained/"
 
